41718225ef2b1c07ca944f6ac61e0168948525f7
darya.sukhova <darya.sukhova@corp.mail.ru>
2021-11-14 13:23:56 +0300

    Initial commit

---
+++ src/main/java/com/example/lab_4_server/Lab4ServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_4_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4ServerApplication.class, args);
+	}
+
+}
